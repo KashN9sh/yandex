c0433bfe3377f2427ed5bb77e3ff76c3dd269b49
KashN9sh <KashN9sh@users.noreply.github.com>
2020-04-17 22:03:30 +0300

    17.04
    Reward function

--- tolApi.py
+++ tolApi.py
@@ -11,12 +11,21 @@ token = 'AgAAAAAPFVH2AAIbujRgrWafYUwrtif00ZzvoJE'
 base = 'https://sandbox.toloka.yandex.ru/api/v1'
 headers = {"Authorization":"OAuth " + token}
 
-def pool_status(id):
-    api_url = base+'/pools/'+str(id)
+def pool_reward(pool_id):
+    api_url = base+'/pools/'+str(pool_id)
+    response = requests.get(api_url, headers=headers)
+    if response.status_code == 200:
+        return response.json()['reward_per_assignment']
+    else:
+       print('Error '+str(response.status_code))
+
+
+def pool_status(pool_id):
+    api_url = base+'/pools/'+str(pool_id)
     response = requests.get(api_url, headers=headers)
     if response.status_code == 200:
        return response.json()['status']
     else:
        print('Error {0}: Content: {1}'.format(response.status_code, response.content))
 
-print(pool_status(364576))
+print(pool_reward(364576))
