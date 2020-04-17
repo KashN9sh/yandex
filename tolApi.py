8842c323b0e1b25372399af32801a4b5deead4c9
KashN9sh <KashN9sh@users.noreply.github.com>
2020-04-17 22:08:39 +0300

    Overlap+
    Чуть все не похерил

--- tolApi.py
+++ tolApi.py
@@ -1,12 +1,6 @@
 import requests
 import json
 
-def u(x):
-        if type(x) == type(b''):
-            return x.decode('utf8')
-        else:
-            return x
-
 token = 'AgAAAAAPFVH2AAIbujRgrWafYUwrtif00ZzvoJE'
 base = 'https://sandbox.toloka.yandex.ru/api/v1'
 headers = {"Authorization":"OAuth " + token}
@@ -26,6 +20,19 @@ def pool_status(pool_id):
     if response.status_code == 200:
        return response.json()['status']
     else:
-       print('Error {0}: Content: {1}'.format(response.status_code, response.content))
+       print('Error '+str(response.status_code))
+
+def task_overlap(pool_id):
+    api_url = base+'/tasks?pool_id='+str(pool_id)
+    response = requests.get(api_url, headers=headers)
+    if response.status_code == 200:
+        overlaps=[]
+        i=0
+        for item in response.json()['items']:
+            overlaps[i]=item["overlap"]
+            i+=1
+        return overlaps
+    else:
+       print('Error '+str(response.status_code))
 
 print(pool_reward(364576))
