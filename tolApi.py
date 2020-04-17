dde960bf943f9d94524dee482aaf22e6089ad8b4
KashN9sh <KashN9sh@users.noreply.github.com>
2020-04-17 22:11:26 +0300

    17.04
    Expire function

--- tolApi.py
+++ tolApi.py
@@ -5,6 +5,14 @@ token = 'AgAAAAAPFVH2AAIbujRgrWafYUwrtif00ZzvoJE'
 base = 'https://sandbox.toloka.yandex.ru/api/v1'
 headers = {"Authorization":"OAuth " + token}
 
+def expire(pool_id):
+    api_url = base+'/pools/'+str(pool_id)
+    response = requests.get(api_url, headers=headers)
+    if response.status_code == 200:
+        return response.json()['will_expire']
+    else:
+       print('Error '+str(response.status_code))
+
 def pool_reward(pool_id):
     api_url = base+'/pools/'+str(pool_id)
     response = requests.get(api_url, headers=headers)
@@ -12,7 +20,6 @@ def pool_reward(pool_id):
         return response.json()['reward_per_assignment']
     else:
        print('Error '+str(response.status_code))
-
 
 def pool_status(pool_id):
     api_url = base+'/pools/'+str(pool_id)
@@ -35,4 +42,4 @@ def task_overlap(pool_id):
     else:
        print('Error '+str(response.status_code))
 
-print(pool_reward(364576))
+print(expire(364576))
